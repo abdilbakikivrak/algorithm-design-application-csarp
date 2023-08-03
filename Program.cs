b889fa6cb476b4933eeb420c57022334c30de0a0
abdilbakikivrak <72791702+abdilbakikivrak@users.noreply.github.com>
2023-08-03 19:09:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp // Note:gerçek ad alanı prıje alanına bağlıdır.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
