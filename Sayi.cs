34870bf3e95f2a3de5fcd97b842032fdd7903bd5
abdilbakikivrak <72791702+abdilbakikivrak@users.noreply.github.com>
2023-08-07 19:10:51 +0300

    Add files via upload

---
+++ Sayi.cs
@@ -0,0 +1,238 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csarpalgorithmdesign3
+{
+    public class Sayi
+    {
+        /// <summary>
+        ///  Parametre olarak aldigi sayinin tek mi cift mi oldunu kontrol eder
+        /// </summary>
+        /// <param name="n">Sayi</param>
+        /// <returns>Tek ise true aksi durumda false donus yapar.</returns>
+        public static bool TekMi(int n)
+        {
+            if (n % 2 == 1)
+            {
+                return true;
+
+            }
+            return false;
+        }
+        /// <summary>
+        /// Parametre olarak aldığı sayinin cift olup olmadığını kontrol eder.
+        /// </summary>
+        /// <param name="n">Girilen Sayi</param>
+        /// <returns>n değeri çift ise  true aksi durumda false dönüs yapar.</returns>
+        public static bool CiftMi(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Parametre olarak aldığı sayinin mutlak değeri döner
+        /// </summary>
+        /// <param name="n">Girilen sayiyi temsil eder</param>
+        /// <returns>Girilen sayinin mutlak  değeri dönüş ifadesi olacak.</returns>
+        public static int MutlakDeger(int n) // girilen n sayisinin mutlak değerini buşan program fonksiyonu 
+        {
+            if (n > 0)
+                return n;
+            else if (n < 0)
+                return -1 * n;
+            else
+                return 0;
+        }
+        /// <summary>
+        /// Girilen değerin asal sayı olup olmadığını kontrol eder
+        /// </summary>
+        /// <param name="n">Girilen sayiyi temsil eder </param>
+        /// <returns>Girilen sayinin asal ifadesinin dönüşü olur</returns>
+        public static bool AsalMi(int n)
+        {
+            if (n <= 1)
+            {
+                Console.WriteLine("En küçük asal sayi 2'dir.");
+                return false;
+            }
+            bool kontrol = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) //  burada yani girilen n sayısının kendinden küçük bir sayıya bölündüğü için asal sayı değil ve kontrol durumunu false yapıyoruz
+                {
+                    kontrol = false;
+                    break;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        ///  Parametre olarak aldığı sayinın rakamları toplamini doner 
+        /// </summary>
+        /// <param name="n">Girilen sayiyi temsil eder.</param>
+        /// <returns>Rakamların toplamdir.</returns>
+        public static int RakamlariToplami(int n)
+        {
+            int toplam = 0, rakam = 0;
+            while (n > 0)
+            {
+                rakam = n % 10;
+                toplam += rakam;
+                n = n / 10;
+
+            }
+            return toplam;
+        }
+        /// <summary>
+        /// i'nin değerini bir artırıp n'e kadar bir fazlasıyla toplayarak n'e kadar olan sayıların toplamını bulmak 
+        /// </summary>
+        /// <param name="n">Girilen sayiyi temsil eder</param>
+        /// <returns>Girilen sayi n'e kadar olan sayiların toplamını döndürür</returns>
+        public static int NeKadarOlanSayilarinToplami(int n)
+        {
+            /*
+            int t = 0;
+           
+            for ( int İ =1; İ <=n; İ++)
+            {
+                t += İ; //standart kalıp i'nin değerini bir artırıp n'e kadar bir fazlasıyla toplayarak n'e kadar olan sayıların toplamını bulmak 
+            }
+            return t;
+            */
+            //formul ile birden n e kadar olan sayiların toplamnın bulmak 
+            return n * (n + 1) / 2; //  görüldüğü gibi yukarıdaki yöntemle 8 satırda yaptıraln işlemi förmül ile tek küçük satırda aynı işlemi yaptırdım.
+        }
+        /// <summary>
+        /// i'nin değerini bir arttıırıp i=1'den n'e kadar olan tek sayıların toplamını hesaplar
+        /// </summary>
+        /// <param name="n">Girilen n sayisini temsil eder</param>
+        /// <returns>Girilen sayiyi n'e kadar olan tek sayiların toplamını döndürür</returns>
+        public static int NeKadarOlanTekSayilarinToplami(int n)
+        {
+            int t = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    t += i;
+                }
+            }
+            return t;
+        }
+        /// <summary>
+        /// 1'den n'e kadar olan tek sayıların !den başlayarak 2 aratarak for döngüsünde tek sayıların toplamını hesaplar
+        /// </summary>
+        /// <param name="n">Girilen n sayiyi ifade eder</param>
+        /// <returns>1'den n'e kadar olan tek sayıların !den başlayarak 2 aratarak for döngüsünde toplam t değerini döndürür </returns>
+        public static int NeKadarOlanTekSayilarinToplamiAlternatifCozum(int n)// favori işlemim =) 
+        {
+            int t = 0;
+            for (int i = 1; i <= n; i += 2)// Burada i=1 değerinden başladıktan sonra her bir döngüde i değerine 2 ekleyerek 1'den sonraki tek sayıları saydırmış olacağız (gerçekten çok zekice fsdfsd)
+            {
+                t += i; // burada da bizim toplamımızı ifade eden t değerine de döngüde birden iki arttırıp saydırdığımız her tek sayıyı da t değerinin üzerine " += " ekleyerek  i<=n 'e kadar olan tek sayilari topluyoruz.
+            }
+            return t;
+        }
+        /// <summary>
+        /// 1'den n'e kadar olan tek sayiların förmüle ile hesaplanması 
+        /// </summary>
+        /// <param name="n">Girilen n sayisini ifade eder</param>
+        /// <returns>!'den n'e kadar olan tek sayıların toplam değerini döndürür.</returns>
+        public static int NeKadarOlanTekSayilarinFormulİleToplami(int n)// bu yöntem de favuri :)
+        {
+            n = n + 1; // n++; // alınan n değerini bir arttırdık 
+            n = n / 2; // daha sonra bir arttırdığımız n değerini 2'ye böldük 
+            return n * n; // ve iki böldükten sonraki n değerinin karesini aldık. 
+            // böylelikle örnek n=5 için =5+1=6/2=3*3=9 yani 1+3+5= 9 'a karşılık gelmektedir.
+        }
+        /// <summary>
+        /// 1'den n'e kadar olan çift sayıların 1'den 1'er artırarak if koşulu ile 2'ye kalansız bölünen n 'e kadar olan sayilar, toplar
+        /// </summary>
+        /// <param name="n">Girilen n saiysini temsil eder</param>
+        /// <returns>1'den başlayarak n'e kadar olan sayıları birer artırarak if dönğüsüyle 2'ye kalansız bölündüğünü kontrol eder ve sağlayanların toplam değerini döndürür.</returns>
+        public static int NeKadarOlanCiftSayilarinToplami(int n)
+        {
+            int t = 0;
+            for (int i = 2; i <= n; i++) // İ=2'den başlayarak n'e kadar birer artırarak döngüyü kurar
+            {
+                if (i % 2 == 0) // i değerinin 2 'den kalanının 0 koşulunu konrol eder 
+                {
+                    t += i;   // eğer if konuşu sağlanır ve birer artarak devam eden döngü içinde 2'ye kalansız bölünen i değeri olursa toplam ifademiz olan t değerinin üzerine i değerini ekleyerek güncel t değerini elde ederiz.
+
+                }
+            }
+
+            return t;
+        }
+        /// <summary>
+        /// 1 den n'kadar olan çift sayıların toplamını alternatif yöntemle bulunması
+        /// </summary>
+        /// <param name="n">Girilen n sayisini ifade eder.</param>
+        /// <returns>1'deb başlayarak n'e kadar olan çift sayıları 2 şer şekilde  for döngüsünde saydırarak toplam t değeri döndürür.</returns>
+        public static int NeKadarOlanCiftSayilarinToplamiAlternatifCozum(int n)
+        {
+            int t = 0;
+            for( int i=2;i<=n; i +=2)
+            {
+                t += i;
+            }
+            return t;
+        }
+        /// <summary>
+        /// 1'den n'e kaddar olan çift sayıları 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int NeKadarOlanCiftSayilarinFormulleToplami(int n)
+        {
+            n = n / 2; // klavyeden aldığımız sayıyı ikiye böldük
+            return n * (n + 1); // daha sonra  ikiye bölünen n değerini n*(n+1) förmülü ile n'e kadar olan çift sayıların toplamını bulduk
+        }
+
+        public static double pi = Math.PI; // matematik kütüphanesiden pi 'nin değerini çekiyoruz
+
+        public static double Cevresi(double r) // Burada consoleread ile aldığımız r değerini çekip döndürdüğümüz değeri Cevresi()  cevresi değişkenine veriyoruz 
+        {
+            return 2 * pi * r;  // işlemi yaparak sonucunu döndürüyoruz 
+        }
+    }
+}
+
+
+
+
+
+/*
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csarpalgorithmdesign3
+{
+    public class Sayi
+    {
+        /// <summary>
+        ///  Parametre olarak aldigi sayinin tek mi cift mi oldunu kontrol eder
+        /// </summary>
+        /// <param name="n">Sayi</param>
+        /// <returns>Tek ise true aksi durumda false donus yapar.</returns>
+        public static bool TekMi(int n)
+        {
+            if (n % 2 == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+*/
---
+++ algorithmdesign2orta.cs
@@ -0,0 +1,185 @@
+﻿/*
+
+// Klavyeden girilen sayinin tek mi cift mi olduğunu public class fonsiyonu çağırılarak işleme alınması 
+using csarpalgorithmdesign3;
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi girin:");
+           int n= Convert.ToInt32( Console.ReadLine());
+            int m = 4;
+            if (Sayi.TekMi(n)) //  oluşturduğumuz sayi.cs sınıfında bu işlemi yapan TekMi() foksiyonunu buraya çağırıp kullanıyoruz 
+            {
+                Console.WriteLine("Grilen sayi= {0} tektir.", n);
+
+
+            }
+            if (Sayi.CiftMi(n)) // bu çalışmanın  oluşturduğumuz  sayi.cs sınıfında bu işlemi yapan CiftMi() foksiyonunu buraya çağırıp kullanıyoruz 
+            {
+                Console.WriteLine("Girilen sayi = {0} çifttir.", n,m);  
+            }
+        }
+    }
+}
+*/
+/*
+// Class fonk kullanılarak girilen sayinin mutlak değerini buluyoruz
+
+using csarpalgorithmdesign3;
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mutlak Deger İçin sayi giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("|{0}|={1}",n, Sayi.MutlakDeger(n));
+            
+        }
+    }
+}
+*/
+/*
+//Asal sayi olup olmadığını sınıf  fonsiyonu ile bulan program
+using csarpalgorithmdesign3;
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asal Sayi kontrolü İçin sayi giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", Sayi.AsalMi(n) ? "Asal Sayidir." : "Asal Sayi değildir.!");// AsalMi(n) sınıf fonksiyonundaki asal sayi kontrol fonksiyonuna girilen n değerini kontrol ettirip ekrana yazırıyoruz.
+                                                // n, Sayi.AsalMi(n)? "Asal Sayidir.": "Asal Sayi değildir.") bu ifade de ?'den hemen sonraki durumu true sonraki ikinci durumu false olarak değerlendirip ekrana yazacaktır.
+        }                                       // soru işaretinden önceki ifade true ise soru işaretinden sonraki ifade true olur yazılır , false ise soru işaretinden iki soneaki ifade false olarak yazılır
+    }
+}
+*/
+/*
+//Girilen Sayinin rakamları toplamını class fonsiyonu ile bulan program
+using csarpalgorithmdesign3;
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rakamları Toplami icin sayi giriniz.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} Sayisinin Rakamları Toplamı = {1}",n,Sayi.RakamlariToplami(n));// burada ilk kısımda n ile {0} atadığımız yere girilen sayıyı yazıp daha sonra {1} ile yazdığımız class fonksiyonu ile n değerinin rakamlarının toplamını yazıyor.
+
+
+        }
+    }
+}
+*/
+
+/*
+// Birden n'e kadar olan sayıların toplamı class fonksiyonu ile hesaplayan işlmeme alan program ###(standar artan toplama yöntemi ve förmül ile hesaplama )
+
+using csarpalgorithmdesign3;
+using System;
+
+namespace ABK
+{//peachypam
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İşlemi Hesaplanacak Sayiyi giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Birden {0}'e kadar olan sayiların       Toplamı ={1,5}", n, Sayi.NeKadarOlanSayilarinToplami(n));
+            Console.WriteLine("Birden {0}'e kadar olan  tek sayiların  Toplamı ={1,5}", n, Sayi.NeKadarOlanTekSayilarinToplami(n));
+            Console.WriteLine("Birden {0}'e kadar olan  tek sayiların  Toplamı Alternatif Çözm ={1,5}", n, Sayi.NeKadarOlanTekSayilarinToplamiAlternatifCozum(n));
+            Console.WriteLine("Birden {0}'e kadar olan  tek sayiların Förmülle Toplamı ={1,5}", n, Sayi.NeKadarOlanTekSayilarinFormulİleToplami(n));
+            Console.WriteLine("Birden {0}'e kadar olan Çift Sayıların   işlemi :{1}-{2}={3}",n, Sayi.NeKadarOlanSayilarinToplami(n), Sayi.NeKadarOlanTekSayilarinToplami(n), Sayi.NeKadarOlanCiftSayilarinToplami(n));
+            Console.WriteLine("Birden {0}'e kadar olan Çift  sayiların Toplamı ={1,5}", n, Sayi.NeKadarOlanCiftSayilarinToplami(n));
+            Console.WriteLine("Birden {0}'e kadar olan Çift  sayiların Toplamı Alternatif Çözum ={1,5}", n, Sayi.NeKadarOlanCiftSayilarinToplamiAlternatifCozum(n));
+            Console.WriteLine("Birden {0}'e kadar olan Çift sayiların Förmülle Toplamı ={1,5}", n, Sayi.NeKadarOlanCiftSayilarinFormulleToplami(n));
+
+
+            // 50'den 80 kadar olan çift sayiların toplamını bulan program ####################################
+            /*
+            int n = 80;
+            int m = 50; // bu şekilde de ayrı tanımlayabilirdim ama değişkene gerek yok fonksiyon parantezinin içerisine yazmak daha kolay.
+            "///
+
+            int t1 = Sayi.NeKadarOlanCiftSayilarinFormulleToplami(80);
+            int t2 = Sayi.NeKadarOlanCiftSayilarinFormulleToplami(50);
+
+            Console.WriteLine("{0,5}-{1,5}={2,5}", t1, t2, t1 - t2); // yukarıda 106. satırda ekrana gösterilen işlemi bu defa gerçek olarak işleme alındı tanımlanan değerleri çıkarakar istenen aralıktaki sayıların toplamını elde edebildik 
+        }
+    }
+}
+*/
+
+/*
+
+// bir çemberin çevresini ve alanını hesaplayan program
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double pi = Math.PI;
+            Console.WriteLine("Çemberin Yarıçapı r değerini giriniz:");
+            double r =Convert.ToInt32( Console.ReadLine()); // Burada konsoldan alınan string türündeki değeri convert.toint32 deyimi ile double türüne değiştirilebilir yaptım
+            Console.WriteLine("Cevresi:2*{0:F2}*{1:F2}={2:F2}", pi  ,r,2*pi*r);// işlem çıktıısnı ekrana yazdırıyoruz  {0:F2} ifadesi referans alınan {0} değerinin virgülden sonra sadece iki basamağını yazdırır
+            Console.WriteLine("Alanı:{0:F3}*{1:F3}*{1:F3}={2:F2}", pi, r, pi * r*r);
+        }
+
+    }
+}
+*/
+
+// Yarı çapı ve açısı girilen çember diliminin alanını hesaplayan denklem ve program
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI; // pi değerini atadık 
+            Console.WriteLine("Çemberin Yarıçapı 'r'  değerini  giriniz:");
+            double r = Convert.ToInt32(Console.ReadLine()); // yarı çap değerini aldık ve string türünden doulble türüne değiştirmesi için de convertToint32 düğümü kullandık
+            Console.WriteLine("Çemberin yaptığı açı 'aci'  değerini  giriniz:");
+            double aci = Convert.ToInt32(Console.ReadLine()); // Çember diliminin yaptığı açı  değerini aldık ve string türünden doulble türüne değiştirmesi için de convertToint32 düğümü kullandık
+            Console.WriteLine("Alanı:{0:F4}*{1}*{1}*{2}/360={3:F5}", pi, r,aci,  pi * r*r*aci/360); // burada çebner diliminin alanını hesaplayan denklemi yazdık A=pi*r*r*aci/360
+            //Console.WriteLine(pi);
+        }
+    }
+}
+
+/*
+using System;
+
+namespace ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+*/
