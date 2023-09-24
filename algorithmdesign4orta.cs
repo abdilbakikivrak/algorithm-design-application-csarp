0b965e7e37ffc80f40d4ce8e883c048cc488a08e
abdilbakikivrak <72791702+abdilbakikivrak@users.noreply.github.com>
2023-09-24 21:49:56 +0300

    Add files via upload

---
+++ algorithmdesign4orta.cs
@@ -0,0 +1,87 @@
+﻿/*
+using System;
+
+namespace abdil // Note:gerçek ad alanı prıje alanına bağlıdır.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=65; i<= 90; i++)
+            {
+                Console.Write(" {0}", (char)i); // (char)i şeklinde yazdığımız zaman program 65'den 90'a kadar olan rakamların ascii karşılıklarını yazdırır 
+
+                if (i%5==0) // burada ise çıktılar 5'er 5'er ayrılarak bir alt satırdan devam etmesi için if koşulu ile ayırırız 
+                {
+                    Console.WriteLine("");// 
+                }
+            }
+           
+        }
+    }
+}
+*/
+
+
+using algorithmdesign4orta; 
+using algorithmdesign3ortaa.cs; // burada using .. diyerek diziler class'ını sonradan bu projeye dahil edip kullanıyorum ya da dizi.cs içerisine gidiğ en baştaki namespace tanımını program tanımıyla aynı yaparak sağlayabiliriz 
+using System;
+
+namespace abdil // Note:gerçek ad alanı prıje alanına bağlıdır.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string1.Desen1('*', 5);// burada string1 class'ı içinde tanımladığımız Desen1 fonksiyonu ile char türünde girdiğimiz deseni tanımladığımız adette fonksiyonda tanımlı olduğ şekil  deseni çıkarıyor 
+            string1.Desen1(); // normalde hiç parametre almayan bir methot yok ancak burada hiç parametre olmamasına rağmen çalışacak çünkü tanımladığım string1 class'ında default olarak çalıştırabileceği char c ve  int n değerleri tanımlı  
+            string1.Desen1('!');// buradaki methot ise char  karakteri  olarak verilen yeni parametreyi ! baz alır , n değerini (basamak değeri) default tanımdan devam eder 
+            string1.Desen1('?', 10);// burada ise hem char türü hem de int n değerlerinin parametrelerini yeni girilene desen1 fonsksiyonunu çalıştırır göre çalıştırır 
+
+            string1.Desen2();
+            string1.KarakterSeti();
+            string1.KarakterSeti(128,255);
+            string1.KarakterSeti(128,255,10);
+
+            
+
+            // String Fonksiyonları
+
+            string ifade = " baki ";
+            Console.WriteLine(ifade);
+            Console.WriteLine(ifade.Length);
+            Console.WriteLine(ifade.Trim());
+            Console.WriteLine(ifade.Trim().Length);
+            Console.WriteLine(ifade.TrimStart());
+            Console.WriteLine(ifade.TrimEnd());
+            Console.WriteLine(ifade.ToLower());
+            Console.WriteLine(ifade.ToUpper());
+            Console.WriteLine(ifade.Replace('b','a')); // b rakamını a rakamı ile değiştir
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+/*
+using System;
+
+namespace MyApp // Note:gerçek ad alanı prıje alanına bağlıdır.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
---
+++ string1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmdesign4orta
+{
+    internal class @string1
+    {
+
+        /// <summary>
+        /// Ters üçgen desen oluşturucu blok 
+        /// </summary>
+        /// <param name="c">Karakter tanımı</param>
+        /// <param name="n">Tekrar Sayısı </param>
+        public static void Desen1(char c='*', int n=5 )
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("{0,10}", new string(c, i)); // "{0,10}" 0-> il tanımı temsil ediyor / 10 ise satırda 10 karakter boşluk ayrıldığını ve bu 10 karakter alanın sağ tarafından itibaren doldurarak $ yazar 
+
+            }
+        }
+
+        /// <summary>
+        ///  Desen oluiturucu 2
+        /// </summary>
+        /// <param name="c">Karakter</param>
+        /// <param name="n">Döngü Sayısını</param>
+        public static void Desen2(char c='?', int n =8)
+        {
+            for (int x=n; x>= 0; x--)
+            {
+                Console.WriteLine("{0,10}", new string(c, x));
+            }
+        }
+
+        /// <summary>
+        /// Karakter Seti belirleme 
+        /// </summary>
+        /// <param name="baslangincindisi"></param>
+        /// <param name="bitisindisi"></param>
+        public static void KarakterSeti(int baslangincindisi=65 , int bitisindisi=90 , int satirkaraktersayisi=5)
+        {
+            for (int i = baslangincindisi; i <= bitisindisi; i++)
+            {
+                Console.Write(" {0}", (char)i); // (char)i şeklinde yazdığımız zaman program 65'den 90'a kadar olan rakamların ascii karşılıklarını yazdırır 
+
+                if (i % satirkaraktersayisi == 0) // burada ise çıktılar 5'er 5'er ayrılarak bir alt satırdan devam etmesi için if koşulu ile ayırırız 
+                {
+                    Console.WriteLine("");// 
+                }
+            }
+        }
+    }
+}
